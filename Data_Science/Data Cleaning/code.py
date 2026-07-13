160f784b513ead88104c09e97da367e3564d3ffc
Pranav Nimje <pranavnimje7@gmail.com>
2026-07-14 01:10:23 +0530

    google colab file

--- Data_Science/Data Cleaning/code.py
+++
@@ -1,9 +0,0 @@
-#Impute missing values
-data['SepalWidthCm'] = data['SepalWidthCm'].fillna(data['SepalWidthCm'].median())
-data['PetalLengthCm'] = data['PetalLengthCm'].fillna(data['PetalLengthCm'].median())
-
-#Drop any remaining nulls
-data = data.dropna() 
-
-print("Null values after cleaning:")
-display(data.isnull().sum())
