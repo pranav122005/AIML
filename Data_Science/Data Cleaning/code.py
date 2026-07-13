e84bd7ad7ba7a84e14450ebac14c0576695ef6a0
Pranav Nimje <pranavnimje7@gmail.com>
2026-07-14 01:00:05 +0530

    Update code.py

--- Data_Science/Data Cleaning/code.py
+++ Data_Science/Data Cleaning/code.py
@@ -1,8 +1,8 @@
-// Impute missing values
+#Impute missing values
 data['SepalWidthCm'] = data['SepalWidthCm'].fillna(data['SepalWidthCm'].median())
 data['PetalLengthCm'] = data['PetalLengthCm'].fillna(data['PetalLengthCm'].median())
 
-// Drop any remaining nulls
+#Drop any remaining nulls
 data = data.dropna() 
 
 print("Null values after cleaning:")
